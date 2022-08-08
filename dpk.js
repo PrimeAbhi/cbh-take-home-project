b76c77dd7f3f2461f731ce3a72cd6e742cd15f09
Abhishek Gupta <gupta.abhi25@live.com>
2022-08-09 00:13:42 +0530

    Update dpk.js

--- dpk.js
+++ dpk.js
@@ -5,24 +5,26 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
+  function getData(data) {
+    let tempData = crypto.createHash("sha3-512").update(data).digest("hex");
+    return tempData;
+  }
+
   if (event) {
     if (event.partitionKey) {
       candidate = event.partitionKey;
+      if (candidate) {
+        if (typeof candidate !== "string") {
+          candidate = JSON.stringify(candidate);
+        }
+      }
     } else {
       const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+      candidate = getData(data);
+      if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+        candidate = getData(candidate);
+      }
     }
   }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return candidate ? candidate : TRIVIAL_PARTITION_KEY;
 };
